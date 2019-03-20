3e1187790c0a2207627b60e60451adc3592d81df
Emerson Ford <emerson.ford@utah.edu>
2019-03-20 14:41:34 -0600

    Including default secrets.py

---
+++ secrets.py
@@ -0,0 +1,16 @@
+# Password for bconsole
+bconsole_password = ""
+
+# CHPC MySQL Database Login
+sql_username = ""
+sql_password = ""
+
+# CHPC GitLab Password
+gitlab_username = ""
+gitlab_password = ""
+
+# MySQL DB Settings
+DB_HOST = ""
+DB_PORT = 3306
+DB_NAME = ""
+
